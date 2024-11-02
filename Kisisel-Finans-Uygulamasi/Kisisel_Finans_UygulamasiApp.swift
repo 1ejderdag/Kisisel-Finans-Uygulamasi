94a88981f2255a630ec14493c723a5421dc7be8e
Ejder Dag <ejdrdag@gmail.com>
2024-11-02 19:06:35 +0300

    Initial Commit

---
+++ Kisisel-Finans-Uygulamasi/Kisisel_Finans_UygulamasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kisisel_Finans_UygulamasiApp.swift
+//  Kisisel-Finans-Uygulamasi
+//
+//  Created by Ejder Dağ on 2.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Kisisel_Finans_UygulamasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
